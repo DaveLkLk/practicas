ec78b28ca03fff5d6931bd792823257f2a429c92
DaveLkLk <blueplanet98dev@gmail.com>
2023-07-19 19:07:42 -0500

    agregando foreach al arr

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-const arr = ['element1', 'element2', 'element3', 'element4', 'element5',]
+const arr = ['element1', 'element2', 'element3', 'element4', 'element5']
+
+arr.forEach(element => {
+    console.log("contenido: ", element);
+})
